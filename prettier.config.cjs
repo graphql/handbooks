f649045501423119c6f329a37253de2d58d36747
Benjie Gillam <benjie@jemjie.com>
2025-11-06 13:15:45 +0000

    Simplify prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,15 +1,5 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  printWidth: 80,
-  proseWrap: 'always',
-  singleQuote: true,
-  trailingComma: 'es5',
-  overrides: [
-    {
-      files: ['*.md', '*.mdx'],
-      options: {
-        proseWrap: 'always',
-      },
-    },
-  ],
+  // Use prettier's defaults, with proseWrap enabled
+  proseWrap: "always",
 };
